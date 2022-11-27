417a55d20d1488003b14a8e0cebeda1846b1df99
Nursultan Askarbekuly <nursultan@outlook.com>
2022-11-27 21:41:08 +0300

    Initial Commit

---
+++ Hani/HaniApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaniApp.swift
+//  Hani
+//
+//  Created by Nursultan Askarbekuly on 27.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct HaniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
